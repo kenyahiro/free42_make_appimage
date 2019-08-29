3f8c0b1f892bbb511f67588740803a827dbf9d04
ken yahiro <ken.yahiro@gmail.com>
2019-08-29 16:41:59 +0900

    スクリプトの中身を書きました。 ドキュメントを追加しました。

--- README.md
+++ README.md
@@ -1,3 +1,27 @@
 # free42 make appimage
 
 Free42のAppImageを作成するスクリプトです。
+
+このスクリプトは以下の動作を行います。
+
+## 必要なファイルのダウンロード。
+
+https://thomasokken.com/free42/ からlinux用のバイナリをダウンロードします。
+また、 ソースコードもダウンロードします。
+
+## ダウンロードしたファイルの展開
+
+上でダウンロードしたtgzを展開します。
+
+## AppImage用のディレクトリの作成
+
+AppImage用のディレクトリを作成して、 必要なファイルをコピーします。
+
+## linuxdeployのダウンロード
+
+https://github.com/linuxdeploy/linuxdeploy からAppImageファイルをダウンロードします。
+ダウンロードしたファイルに実行属性をつけます。
+
+## AppImageの作成
+
+上でダウンロードしたlinuxdeployを使って、AppImageを作成します。
--- make_appimage.sh
+++ make_appimage.sh
@@ -1,1 +1,34 @@
 #!/bin/bash
+
+# download binary file.
+if [ ! -f Free42Linux.tgz ]; then
+    wget https://thomasokken.com/free42/download/Free42Linux.tgz
+fi
+
+# download source file.
+if [ ! -f free42.tgz ]; then
+    wget https://thomasokken.com/free42/download/free42.tgz
+fi
+
+tar xvf Free42Linux.tgz
+tar xvf free42.tgz
+
+app_dir=Free42.AppDir
+
+mkdir -p ${app_dir}
+mkdir -p ${app_dir}/usr
+mkdir -p ${app_dir}/usr/bin
+
+cp AppRun ${app_dir}
+cp free42.desktop ${app_dir}
+cp free42/gtk/icon-128x128.xpm ${app_dir}
+cp Free42Linux/free42bin ${app_dir}/usr/bin
+
+# download linuxdeploy-x86_64.AppImage file. for create AppImage file.
+if [ ! -f linuxdeploy-x86_64.AppImage ]; then
+    wget https://github.com/linuxdeploy/linuxdeploy/releases/download/continuous/linuxdeploy-x86_64.AppImage
+    chmod +x linuxdeploy-x86_64.AppImage
+fi
+
+# create AppImage file.
+./linuxdeploy-x86_64.AppImage --appdir ${app_dir} --output appimage
